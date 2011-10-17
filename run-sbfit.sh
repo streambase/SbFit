da86f78b6d80f4f70ccd8ba71a818c2ca444192f
haydens@737f8f2c-97f8-0310-ac9b-cfac8cc1a2f5 <haydens@737f8f2c-97f8-0310-ac9b-cfac8cc1a2f5>
2011-10-17 17:18:23 +0000

    made script work with all jars in lib except sbfit.jar uses sbfit.jar rather than java-bin -- HACK ALERT -- don't know why this fails made test for null in UnorderedDequeue cell
    --HG--
    extra : convert_revision : svn%3A737f8f2c-97f8-0310-ac9b-cfac8cc1a2f5/services/Frameworks/SbFit/trunk%40145321

--- run-sbfit.sh
+++ run-sbfit.sh
@@ -11,6 +11,6 @@ SBFIT=java-bin
 SBUNIT_EXT=/Users/haydens/work/mercurial/sbunit-ext
 SBUNIT_EXT_JARS=${SBUNIT_EXT}/build/sbunit-ext.jar:${SBUNIT_EXT}/lib/gson-1.7.1.jar
 
-export CLASSPATH=$SBFIT:fitnesse-20101101.jar:fitlibrary.jar:$STREAMBASE_HOME/lib/sbclient.jar:$STREAMBASE_HOME/lib/junit.jar:$STREAMBASE_HOME/lib/sbtest-unit.jar:$STREAMBASE_HOME/lib/slf4j-api-1.6.1.jar:${SBUNIT_EXT_JARS}
+export CLASSPATH=$SBFIT:lib/fitnesse-20101101.jar:lib/fitlibrary.jar:$STREAMBASE_HOME/lib/sbclient.jar:$STREAMBASE_HOME/lib/junit.jar:$STREAMBASE_HOME/lib/sbtest-unit.jar:$STREAMBASE_HOME/lib/slf4j-api-1.6.1.jar:${SBUNIT_EXT_JARS}
 
 java -Dlogback.configurationFile=sbfit-main-logback.xml fitnesseMain.FitNesseMain -e 0 -p $FIT_PORT
