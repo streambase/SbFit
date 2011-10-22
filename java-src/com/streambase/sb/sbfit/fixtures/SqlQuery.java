be3a1a263d899bef20b56148a6ab78ee4b7c9ed6
Hayden Schultz <haydens@streambase.com>
2011-10-22 15:39:51 -0400

    SqlQuery fixture works

--- java-src/com/streambase/sb/sbfit/fixtures/SqlQuery.java
+++ java-src/com/streambase/sb/sbfit/fixtures/SqlQuery.java
@@ -2,10 +2,15 @@ package com.streambase.sb.sbfit.fixtures;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +22,7 @@ import com.streambase.sb.util.Msg;
 import com.streambase.sb.util.Util;
 
 import fit.ColumnFixture;
+import fit.Fixture;
 import fit.Parse;
 
 public class SqlQuery extends ColumnFixture {
@@ -24,6 +30,10 @@ public class SqlQuery extends ColumnFixture {
 
     private DBUtil dbUtil = new DBUtil();
 	private DataSourceInfo dataSource;
+
+	private Statement statement;
+
+	private Connection con;
 	
     public SqlQuery() {
 	}
@@ -41,13 +51,12 @@ public class SqlQuery extends ColumnFixture {
 		
 		logger.info("SqlQuery from data-source {}, sql query {}", dataSourceName, sqlQueryText);
 
- 
         try {
-			DataSourceInfo ds = dbUtil.getDataSource(dataSourceName);
 			List<String> sqlCommands = new ArrayList<String>();
 			String [] rowNames = findTableHeaders(rows.parts);
 
-			logger.info("header: {}", rows.parts.text());
+			dataSource = dbUtil.getDataSource(dataSourceName);
+
 			Parse row = rows;
 
 			while((row=row.more) != null) {
@@ -59,15 +68,29 @@ public class SqlQuery extends ColumnFixture {
 				} while((column = column.more) != null);
 			}
 
-			result = executeSql(ds, sqlQueryText);
+			result = executeSql(dataSource, sqlQueryText);
 			
 			logger.info("DB query returned");
-			
-			compareResultToTable(row, result, rowNames);
+
+			compareResultToTable(rows, result, rowNames);
 		} catch (Exception e) {
 			logger.error("SqlScript", e);
 			exception(rows, e);
 		}
+		finally {
+			if(statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+				}
+				statement = null;
+			}
+			if(con != null) {
+				dataSource.releaseConnection(con);
+				con = null;
+			}
+		}
+
 	}
 	
 	public String [] findTableHeaders(Parse headerCells) {
@@ -84,23 +107,112 @@ public class SqlQuery extends ColumnFixture {
 	}
 
 	private void compareResultToTable(Parse row, ResultSet result, String [] fieldNames) throws SQLException {
-        boolean resultDone = false;
+        boolean more = true;
+        Map<String, Integer> mapping = findColumnNameMapping(result);
+        Parse oldRow = row;
+        
+        more = result.first();
         
         while((row=row.more) != null) {
             Parse cell = row.parts;
             
-        	for (int column = 0; column < fieldNames.length; column++, cell = cell.more) {
+            for(String columnName : fieldNames) {
         		String expected = cell.text();
-        		String actual = result.getString(fieldNames[column]); 
+        		String found = null;
+        		boolean matched = false;
+        		
+        		logger.debug("looking for column {}", columnName);
 
-        		if(!resultDone && actual.equals(expected)) {
-        			right(cell);
-        		} else {
+        		if(!more) {
+        			// the result set returned less rows than we were expecting
         			wrong(cell);
+        		} else {
+        			if(isFloatingPoint(mapping.get(columnName))) {
+        				double actual = result.getDouble(columnName);
+        				double exp = Double.parseDouble(expected);
+
+        				matched = Util.compareDoubles(exp, actual);
+        				found = Double.toString(actual);
+        				logger.debug(MessageFormat.format("expected double: {0}, actual {1} matched {2}", exp, actual, matched));
+        			} else {
+        				String actual = result.getString(columnName); 
+
+        				matched = actual.equals(expected);
+        				found = actual;
+        				logger.debug(MessageFormat.format("expected string: {0}, actual {1} matched {2}", expected, actual, matched));
+        			}
+
+        			if(matched) {
+        				right(cell);
+        			} else {
+        				wrong(cell, found);
+        			}
         		}
-        		resultDone = result.next();
-        	}
+
+        		cell = cell.more;
+            }
+            
+            oldRow = row;
+            if(more) {
+            	more = result.next();
+            }
         }
+        
+        if(more) {
+        	addUnexpectedRows(oldRow, result, fieldNames);
+        }
+	}
+	
+    private void addUnexpectedRows(Parse priorRow, ResultSet result, String [] columnNames) throws SQLException {
+        do {
+        	addUnexpectedRow(priorRow.last(), result, columnNames);
+        } while(result.next());
+    }
+    
+    private void addUnexpectedRow(Parse priorRow, ResultSet result, String [] columnNames) {
+        Parse lastRow = priorRow;
+        Parse newRow = new Parse("tr", null, null, null);
+        lastRow.more = newRow;
+        lastRow = newRow;
+
+        try {
+        	Parse cell = new Parse("td", "", null, null);
+        	String colName = columnNames[0];
+
+        	cell.addToBody(Fixture.gray("? = " + result.getString(colName)));
+        	ignore(cell);
+        	newRow.parts = cell;
+
+        	for (int column = 1; column < columnNames.length; column++) {
+        		colName = columnNames[column];
+        		Parse current = new Parse("td", "", null, null);
+        		current.addToBody(Fixture.gray("? = " + result.getString(colName)));
+        		ignore(current);
+        		cell.more = current;
+        		cell = current;
+        	}
+        } catch (Exception e) {
+        	exception(newRow, e);
+        } catch (Throwable e) {
+        	e.printStackTrace();
+        }
+    }
+
+	
+	private boolean isFloatingPoint(int type) {
+		return type == Types.FLOAT || type == Types.DOUBLE;
+	}
+
+	/** @return map of column name to sql type */
+	private Map<String, Integer> findColumnNameMapping(ResultSet result) throws SQLException {
+		ResultSetMetaData meta = result.getMetaData();
+		Map<String, Integer> mapping = new HashMap<String, Integer>();
+		
+		for(int i=0; i < meta.getColumnCount(); ++i) {
+			mapping.put(meta.getColumnName(i+1), meta.getColumnType(i+1));
+		}
+		
+		return mapping;
 	}
 
 	
@@ -112,33 +224,18 @@ public class SqlQuery extends ColumnFixture {
      * @throws StreamBaseException
      */
 	public ResultSet executeSql(DataSourceInfo dataSource, String sql) throws StreamBaseException {
-		Connection con = null;
-        Statement statement = null;
+		con = null;
+        statement = null;
 
-		try {
-			con = dataSource.getConnection(dataSource.numTypecheckReconnectAttempts());
+        con = dataSource.getConnection(dataSource.numTypecheckReconnectAttempts());
 
-            try {
-		        statement = con.createStatement();
-            } catch (SQLException e) {
-                throw new StreamBaseException("Unable to create statement", e);
-            }
+        try {
+        	statement = con.createStatement();
+        } catch (SQLException e) {
+        	throw new StreamBaseException("Unable to create statement", e);
+        }
 
-            return execute(statement, sql.trim());
-		}
-		finally {
-			if(statement != null) {
-				try {
-					statement.close();
-				} catch (SQLException e) {
-				}
-                statement = null;
-            }
-			if(con != null) {
-                dataSource.releaseConnection(con);
-                con = null;
-			}
-		}
+        return execute(statement, sql.trim());
 	}
 
     public void forciblyReleaseConnection() {
@@ -162,7 +259,7 @@ public class SqlQuery extends ColumnFixture {
             return null;
         Msg.debug("sqlscript: " + command);
         try {
-            return statement.executeQuery(command);
+        	return statement.executeQuery(command);
 		} catch (SQLException e) {
 		    throw new StreamBaseException(Msg.format("Exception while executing {0}", Util.quote(command)), e);
 		}
