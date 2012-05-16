4b822beadab0db38939f2c8db6ff0f76724dcffe
Steve Barber <sbarber2@gmail.com>
2012-05-16 17:49:46 -0400

    update logback configuration for encoders

--- run-sbfit.cmd
+++ run-sbfit.cmd
@@ -8,10 +8,12 @@ set SBUNIT_EXT=lib\sbunit-ext
 set SBUNIT_EXT_JARS=%SBUNIT_EXT%\build\sbunit-ext.jar;%SBUNIT_EXT%\lib\gson-1.7.1.jar
 
 echo Running ant build to pick up any java source changes
+set CLASSPATH=
 call ant
 
 mkdir build\sbars
 
-set CLASSPATH="%SBFIT%;lib\fitnesse-20101101.jar;lib\fitlibrary.jar;%STREAMBASE_HOME%\lib\sbclient.jar;%STREAMBASE_HOME%\lib\junit.jar;%STREAMBASE_HOME%;lib\sbtest-unit.jar;%STREAMBASE_HOME%\lib\slf4j-api-1.6.1.jar;%SBUNIT_EXT_JARS%"
+set CLASSPATH="%SBFIT%;lib\fitnesse-20101101.jar;lib\fitlibrary.jar;%STREAMBASE_HOME%\lib\sbclient.jar;%STREAMBASE_HOME%\lib\junit.jar;%STREAMBASE_HOME%;lib\sbtest-unit.jar;%SBUNIT_EXT_JARS%"
 
 java -Dlogback.configurationFile=sbfit-main-logback.xml fitnesseMain.FitNesseMain -e 0 -p %FIT_PORT%
+
